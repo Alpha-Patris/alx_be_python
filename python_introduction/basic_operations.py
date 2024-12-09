7a653f09a4a8e6c9d53535bda84c435402d77fe1
Alpha-Patris <stevemuvui31@gmail.com>
2024-12-09 17:38:31 +0300

    basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+x = number1 + number2
+y = number1 - number2
+z = number1 * number2 
+
+print ("Addition of {} and {} is {}".format (number1, number2, x))
+print ("Subtraction of {} and {} is {}".format (number1, number2, y))
+print ("Multiplication of {} and {} is {}".format (number1, number2, z))
