880841d061d80e64ce9eaa64aaa3f283c915ac98
Alpha-Patris <stevemuvui31@gmail.com>
2025-01-19 21:30:56 +0300

    y

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime("%Y-%m-%d *%H:%M:%S")
     global current_date
     current_date = current_datetime.date()
-    formatted_datetime = current_datetime.strftime("%Y-%m-%d\s*%H:%M:%S")
     print("Current date and time:", formatted_datetime)
     return
 
